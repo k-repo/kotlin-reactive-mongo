360dcfd193a7a1686affcff327569a3a15a29afe
Kamal Azaddyne <akamal@mediametrie.fr>
2018-08-08 11:39:53 +0200

    adding stepVerifier

--- src/test/kotlin/com/example/kotlinreactivemongo/KotlinReactiveMongoApplicationTests.kt
+++ src/test/kotlin/com/example/kotlinreactivemongo/KotlinReactiveMongoApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinreactivemongo
 
+import junit.framework.Assert.assertEquals
 import org.bson.types.ObjectId
 import org.junit.Before
 import org.junit.Test
@@ -10,6 +11,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,6 +32,7 @@ class KotlinReactiveMongoApplicationTests {
     fun testUserRouter() {
         var user = User(ObjectId().toString(), "username1", "password", "username1@mail.com")
 
+        // POST
         webTestClient.post().uri("/users")
                 .accept(MediaType.APPLICATION_JSON_UTF8)
                 .body(Mono.just(user), User::class.java)
@@ -41,10 +44,20 @@ class KotlinReactiveMongoApplicationTests {
                 .jsonPath("$[0].username").isEqualTo("username1")
                 .jsonPath("$[0].email").isEqualTo("username1@mail.com")
 
+        StepVerifier.create(userRepository.findAll())
+                .assertNext {
+                    assertEquals("username1", it.username)
+                    assertEquals("username1@mail.com", it.email)
+                }
+                .expectComplete()
+                .verify()
+
 
         var id0 = userRepository.findAll().toStream().findFirst().get().id
         var user2 = User(id0, "SUPER_USER", "password", "username1@mail.com")
 
+
+        // PUT
         webTestClient.put().uri("/users")
                 .accept(MediaType.APPLICATION_JSON_UTF8)
                 .body(Mono.just(user2), User::class.java)
@@ -57,7 +70,7 @@ class KotlinReactiveMongoApplicationTests {
                 .jsonPath("$[0].email").isEqualTo("username1@mail.com")
 
 
-
+        // GET ALL
         webTestClient.get().uri("/users")
                 .accept(MediaType.APPLICATION_JSON_UTF8)
                 .exchange()
@@ -70,20 +83,11 @@ class KotlinReactiveMongoApplicationTests {
 
         var id = userRepository.findAll().toStream().findFirst().get().id
 
+        // DLEETE
         webTestClient.delete().uri("/users/$id")
                 .accept(MediaType.APPLICATION_JSON_UTF8)
                 .exchange()
                 .expectStatus().isOk
-
-
-        webTestClient.get().uri("/users")
-                .accept(MediaType.APPLICATION_JSON_UTF8)
-                .exchange()
-                .expectStatus().isOk
-                .expectHeader()
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .expectBody()
-                .json("[]")
 
     }
 
